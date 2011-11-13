9bd52d5d9063458c778191329571a563ed29de7f
Ant Zucaro <azucaro@gmail.com>
2011-11-13 18:43:24 -0500

    Add a function for writing the templates to file.

--- generate.py
+++ generate.py
@@ -5,17 +5,23 @@ from glob import glob
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
+def write_template(rt, fn):
+    f = open(fn, 'w')
+    f.write(rt)
+    f.close()
+
 os.chdir(os.getcwd()+'/original')
 images = glob('*jpg')
 images.sort()
 os.chdir('../')
 
-# render the template
+# render and write the main template
 lu = TemplateLookup(directories=['.'])
-t = Template(filename='gallery.mako', lookup=lu)
-rt = t.render(images=images)
+t1 = Template(filename='index.mako', lookup=lu)
+rt1 = t1.render(num_pages=1)
+write_template(rt1, 'test_index.html')
 
-# write the template
-f = open('test_gallery.html', 'w')
-f.write(rt)
-f.close()
+# render the gallery template
+t2 = Template(filename='gallery.mako', lookup=lu)
+rt2 = t2.render(images=images)
+write_template(rt2, 'test_gallery.html')
