5396fe301991c0d43face8273fb2dc3b2ca21487
Ant Zucaro <azucaro@gmail.com>
2011-11-13 19:50:34 -0500

    Get pagination right.

--- generate.py
+++ generate.py
@@ -17,20 +17,20 @@ images.sort()
 os.chdir('../')
 
 current_page = 1
-images_per_page = 25
-total_pages = ceil(len(images)/float(images_per_page))
+images_per_page = 24
+total_pages = ceil(float(len(images))/float(images_per_page))
 
 # render and write the main template
 lu = TemplateLookup(directories=['.'])
 t1 = Template(filename='index.mako', lookup=lu)
-rt1 = t1.render(num_pages=1)
+rt1 = t1.render(num_pages=total_pages)
 write_template(rt1, 'index.html')
 
 start = 0
 for i in range(total_pages):
     end = start+images_per_page
     if end > len(images):
-        end = len(images)
+        end = len(images)+1
     print "Start: {0}. End: {1}".format(start, end)
 
     # render the gallery template
@@ -38,5 +38,5 @@ for i in range(total_pages):
     rt2 = t2.render(images=images[start:end], total_pages=total_pages, current_page=current_page)
     write_template(rt2, '{0}.html'.format(current_page))
 
-    start = end+1
+    start = end
     current_page = current_page+1
